a2ebecb073687f68ca6c5145b1fc1e88e3411469
Azarill <heierms@gmail.com>
2022-05-11 13:59:55 -0500

    initialzed files

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = ('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -7,7 +10,27 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'project-title',
+            message: 'What is the title of your project?',
+            validate: projectTitle =>{
+                if(projectTitle){
+                    return true;
+                }
+                else{
+                    console.log('Please enter a title for your project');
+                    return false;
+                }
+            }
+        },
+        {
+            
+        }
+    ])
+}
 
 // Function call to initialize app
 init();
